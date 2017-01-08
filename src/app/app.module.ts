ed48f5bc201054437a22d1c9c355215500468958
Aleksey Shurygin <magnito12@yandex.ru>
2017-01-08 17:49:34 +0700

    ?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 
+import { CatalogComponent } from './catalog/catalog.component';
+import { MainPageComponent } from './main-page/main-page.component'
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: '', component: MainPageComponent},
+      {path: 'catalog', component: CatalogComponent}
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent ]
 })
 export class AppModule { }
+
+platformBrowserDynamic()
